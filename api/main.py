8eafc722aa6be62588fcd961225fc0e2a86997e8
rishinusathsara21-cmd <rishinusathsara21@gmail.com>
2026-02-09 20:23:48 +0530

    Final successful deployment

--- api/main.py
+++ api/main.py
@@ -3,9 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "BuildXAI is Live!"
-
-# මේ පේළිය අනිවාර්යයෙන්ම තියෙන්න ඕනේ
-if __name__ == "__main__":
-    app.run()
+def index():
+    return "BuildXAI is officially LIVE!"
