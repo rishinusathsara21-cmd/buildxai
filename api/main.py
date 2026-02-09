aca9d256859bee6949a51c0c5936be082246a515
rishinusathsara21-cmd <rishinusathsara21@gmail.com>
2026-02-09 19:54:57 +0530

    Final fix for 500 error

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from BuildXAI!"
+def home():
+    return "BuildXAI is Live!"
+
+@app.route('/api/main')
+def main_api():
+    return "Hello from BuildXAI API!"
 
 if __name__ == "__main__":
     app.run()
